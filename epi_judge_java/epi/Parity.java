de2a21931bc8fa26aecb1ffaee099976c26254b9
garcia-eddie <eddiegarcia.cs@gmail.com>
2023-03-08 21:59:10 -0600

    Add Parity Solution

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short parity = 0;
+
+    while (x != 0) {
+      x &= x - 1;
+      parity ^= 1;
+    }
+
+    return parity;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
