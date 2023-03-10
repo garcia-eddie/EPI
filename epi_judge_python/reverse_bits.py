a660ac81360ea74ab5525666dda689afdca38acc
Eddie Garcia-Alamilla <eddiegarcia.cs@gmail.com>
2023-03-10 17:24:13 -0600

    Add Reverse Bits Solution

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,9 +2,20 @@ from test_framework import generic_test
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    bitmask = 0xFFFF
+    firstQuarter = reverse((x >> 48) & bitmask)
+    secondQuarter = reverse((x >> 32) & bitmask)
+    thirdQuarter = reverse((x >> 16) & bitmask)
+    fourthQuarter = reverse(x & bitmask)
 
+    return firstQuarter | (secondQuarter << 16) | (thirdQuarter << 32) | (fourthQuarter << 48)
+
+def reverse(x: int) -> int:
+    x = (x >> 8) | (x << 8)
+    x = (x & 0xF0F0) >> 4 | (x & 0x0F0F) << 4
+    x = (x & 0xCCCC) >> 2 | (x & 0x3333) << 2
+    x = (x & 0xAAAA) >> 1 | (x & 0x5555) << 1
+    return x
 
 if __name__ == '__main__':
     exit(
