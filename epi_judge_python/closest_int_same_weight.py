ba606ceb6d05bf2943e6ea54f0f963b1242c9e41
Eddie Garcia-Alamilla <eddiegarcia.cs@gmail.com>
2023-03-10 17:49:13 -0600

    Add Closest Int Same Weight Solution

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    The weight of a nonnegative integer x is the number of bits that are set to 1 in its 
+    binary representation. For example, 92 in base-2 is 0x01011100, so its weight is 4.
+
+    Write a program that takes as input a nonnegative integer x and returns a number y 
+    which is not equal  to x, but has the same weight as x and their difference |y - x| 
+    is as small as possible. Assume the integer fits in 64 bits.
+    """
+    NUM_UNSIGNED_BITS = 64
+    for i in range(NUM_UNSIGNED_BITS - 1):
+        if ((x >> i) & 1) != ((x >> i + 1) & 1):
+            mask = (1 << i) | (1 << (i + 1))
+            return x ^ mask
+
+    return ValueError('All bits are 0 or 1')
 
 
 if __name__ == '__main__':
