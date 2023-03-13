7ee16d6a4c123c379de09d220820499374ce7cf7
garcia-eddie <eddiegarcia.cs@gmail.com>
2023-03-13 17:02:20 -0500

    Add Primitive Multiply Solution

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    sum = 0
+    while y != 0:
+        if y & 1:
+            sum = add(sum, x)
+        x <<= 1
+        y >>= 1
+    return sum
+
+
+def add(x: int, y: int) -> int:
+    while y != 0:
+        shift = x & y
+        carry = x ^ y
+        x = (shift << 1) | carry
+        y = (shift << 1) & carry
+
+    return x
 
 
 if __name__ == '__main__':
