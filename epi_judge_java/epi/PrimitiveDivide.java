3bbc51402da1e0a44fdd15d50454abb3c6554fce
garcia-eddie <eddiegarcia.cs@gmail.com>
2023-03-13 19:43:30 -0500

    Add Primitive Divide Solution

--- epi_judge_java/epi/PrimitiveDivide.java
+++ epi_judge_java/epi/PrimitiveDivide.java
@@ -1,18 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class PrimitiveDivide {
   @EpiTest(testDataFile = "primitive_divide.tsv")
   public static int divide(int x, int y) {
-    // TODO - you fill in here.
-    return 0;
+    int res = 0;
+    while (x >= y) {
+      int shift = 0;
+      long yPower = y;
+      while (x >= (yPower << shift)) {
+        shift++;
+      }
+      shift--;
+      res += 1 << shift;
+      x -= y << shift;
+    }
+
+    return res;
   }
 
   public static void main(String[] args) {
+    int res = divide(2097428739, 186);
     System.exit(
         GenericTest
             .runFromAnnotations(args, "PrimitiveDivide.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
